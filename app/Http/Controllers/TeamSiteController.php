c5190d2ab6d1a13260420beae53271b45db3879f
Justin Slabbert <justinslabbert85@gmail.com>
2018-09-30 19:44:32 +0200

    First Commit

--- app/Http/Controllers/TeamSiteController.php
+++ app/Http/Controllers/TeamSiteController.php
@@ -53,7 +53,7 @@ class TeamSiteController extends Controller
     public function sitebilling()
     {
 
-        $teamsites = TeamSite::where('website_id', '>', 27)->get();
+        $teamsites = TeamSite::where('website_id', '>', 58)->get();
         foreach($teamsites as $teamSite){
 
 
