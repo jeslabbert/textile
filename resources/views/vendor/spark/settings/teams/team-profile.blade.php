09bfcbd4a8e1446f60d9617337276772b6c20866
Justin Slabbert <justinslabbert85@gmail.com>
2018-12-18 16:51:00 +0200

    First Commit

--- resources/views/vendor/spark/settings/teams/team-profile.blade.php
+++ resources/views/vendor/spark/settings/teams/team-profile.blade.php
@@ -11,8 +11,8 @@
 
             <div class="card card-default"><div class="card-header">
                     Update Team Site -
-                    <button data-toggle="modal" data-target="#sitedns" class="btn btn-link btn-sm pull-right"><i style="color: black;" class="fa fa-info"></i></button>
-                    <a class="btn btn-link btn-sm pull-right" href="http://{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}"><i style="color: black;" class="fa fa-info"></i> Visit Site</a>
+                    {{--<button data-toggle="modal" data-target="#sitedns" class="btn btn-link btn-sm pull-right"><i style="color: black;" class="fa fa-info"></i></button>--}}
+                    {{--<a class="btn btn-link btn-sm pull-right" href="http://{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}"><i style="color: black;" class="fa fa-info"></i> Visit Site</a>--}}
                 </div>
                 <div class="card-body">
                     <form class="form-horizontal" method="POST" action="/updatesite">
@@ -21,16 +21,22 @@
                          <div class="form-group row">
                             <label class="col-md-4 col-form-label text-md-right">Domain Name</label>
                             <div class="col-md-6">
-                                <input id="domainname" type="text" class="form-control" name="domainname" value="{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}" required>
-                                <span class="invalid-feedback" style="display: none;">
-
-                                </span>
+                                <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+                                <div class="input-group mb-2 mr-sm-2">
+                                    <div class="input-group-prepend">
+                                        <a href="http://{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}"><div class="input-group-text"><i style="color: black;" class="fa fa-link"></i></div></a>
+                                    </div>
+                                    <input name="domainname" type="text" class="form-control py-0" id="inlineFormInputGroupUsername2" aria-describedby="dnsHelpBlock" placeholder="URL">
+                                    <small id="dnsHelpBlock" class="form-text text-muted">
+                                        An A record for the domain name is needed. It should point to 154.66.198.90
+                                    </small>
+                                </div>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-4 col-form-label text-md-right">Default Domain Backup</label>
                             <div class="col-md-6">
-                                <input id="domainname" type="text" class="form-control" value="{{App\TeamSite::where('team_id', $team->id)->first()->historical_fqdn}}" readonly>
+                                <input id="domainname" type="text" class="form-control " value="{{App\TeamSite::where('team_id', $team->id)->first()->historical_fqdn}}" readonly>
                             </div>
 
                         </div>
