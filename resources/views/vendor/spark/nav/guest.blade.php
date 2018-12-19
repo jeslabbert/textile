b510d5ed1cdbbf103e84812a7b3e2c34060973cb
Justin Slabbert <justinslabbert85@gmail.com>
2018-12-19 13:36:05 +0200

    First Commit

--- resources/views/vendor/spark/nav/guest.blade.php
+++ resources/views/vendor/spark/nav/guest.blade.php
@@ -27,10 +27,10 @@
                 {{--</g>--}}
             {{--</svg>--}}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
+                {{--aria-expanded="false" aria-label="Toggle navigation">--}}
+            {{--<span class="navbar-toggler-icon"></span>--}}
+        {{--</button>--}}
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
             <ul class="navbar-nav ml-auto">
                 {{--<li class="nav-item">--}}
