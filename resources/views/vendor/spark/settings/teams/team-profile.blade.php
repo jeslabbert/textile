b1fd44f2e8ac9c84f7aaa2b104dfbbbe6f4972e8
Justin Slabbert <justinslabbert85@gmail.com>
2019-11-04 09:06:54 +0200

    First Commit

--- resources/views/vendor/spark/settings/teams/team-profile.blade.php
+++ resources/views/vendor/spark/settings/teams/team-profile.blade.php
@@ -22,7 +22,7 @@
                                     <div class="input-group mb-2 mr-sm-2">
                                         {{--TODO Fix up sizing of input--}}
                                         <div class="input-group-prepend">
-                                            <div class="input-group-text" style="min-height: 38px;"><a href="https://{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}" target="_blank"><i style="color: black;" class="fa fa-link"></i></a></div>
+                                            <div class="input-group-text" style="min-height: 38px;"><a href="https://{{App\TeamSite::where('team_id', $team->id)->first()->fqdn}}" target="_blank" data-toggle="tooltip" title="{{__('teams.visit_site')}}"><i style="color: black;" class="fa fa-link"></i></a></div>
                                         </div>
                                         <input name="domainname" type="text" class="form-control py-0" id="inlineFormInputGroupUsername2" aria-describedby="dnsHelpBlock" placeholder="URL" style="width: auto;">
 
