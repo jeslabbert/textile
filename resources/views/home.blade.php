a7372e0ddb37dbe4b1d4951a86ad0827367e83b5
Justin Slabbert <justinslabbert85@gmail.com>
2020-11-16 12:34:05 +0200

    RunCloud Fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">{{__('Dashboard')}}</div>
+                    <div class="card-header">{{__('Dashboard')}} V1</div>
 
                     <div class="card-body">
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
